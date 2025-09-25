c774eeab36cfabc553649c78f85025ea99c9215d
Vinicius <vinicios9497@hotmail.com>
2025-09-24 23:41:52 -0300

    Update calculo_aplicacao_financeira.py

--- calculo_aplicacao_financeira.py
+++ calculo_aplicacao_financeira.py
@@ -1,17 +1,10 @@
 # Uma instituição financeira necessita de um programa que calcula o valor futuro de uma aplicação financeira com base em diferentes cenários de investimento, como a escolha entre uma conta poupança, um Certificado de Depósito Bancário (CDB) ou Tesouro Direto. O programa deve considerar diferentes taxas de juros e períodos de investimento e será útil para quem quer comparar diferentes opções de investimento e entender como o valor de uma aplicação pode crescer ao longo do tempo com diferentes taxas de juros.
-
 # O programa deve solicitar ao usuário o código do tipo de investimento (1- poupança, 2- CDB ou 3- Tesouro Direto), o valor inicial da aplicação e o prazo do investimento em meses.
-
 # As taxas de juros mensais são definidas para cada tipo de investimento:
-
 # - Poupança: 0,5% ao mês.
-
 # - CDB: 0,8% ao mês.
-
 # - Tesouro Direto: 1% ao mês.
-
 # Este programa deve calcula o valor futuro da aplicação usando a fórmula de juros compostos:
-
 # Valor Futuro = Valor Inicial *  (1  + Taxa de Juros)
 
 tipo_investimento = int(input(
@@ -31,3 +24,4 @@ else:
 
 valor_futuro = valor_inicial * (1 + taxa_juros) ** prazo_meses
 print(f'O valor futuro da aplicação é: R$ {valor_futuro:.2f}')
+
